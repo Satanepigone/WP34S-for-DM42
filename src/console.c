e89dcf8e87bea3f22a55a347d90cf2caf25f1dde
Nigel Dowrick <njdowrick@gmail.com>
2021-06-26 12:10:35 +0100

    Trying to get files the same in both branches! Not there yet.

--- src/console.c
+++ src/console.c
@@ -540,6 +540,7 @@ void program_main(){
       }
       if (remapped.key_34s == K_MULTI) {
 	remapped = do_multi (remapped);
+	clear_disp();
 	display_current_menu();
       }
       if (remapped.key_34s == K_EXIT) break;
